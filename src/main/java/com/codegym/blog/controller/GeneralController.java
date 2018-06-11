54bab648032a4332f3604876dddda48b4a58d224
nguyenxuanhoa94 <nguyenxuanhoa.fullstack@gmail.com>
2018-06-11 22:32:25 +0700

    Add feature edit post

---
+++ src/main/java/com/codegym/blog/controller/GeneralController.java
@@ -0,0 +1,19 @@
+package com.codegym.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GeneralController {
+    @GetMapping("/")
+    public String homePage() {
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/404")
+    public ModelAndView show404Page() {
+        ModelAndView modelAndView = new ModelAndView("/404");
+        return modelAndView;
+    }
+}
--- src/main/java/com/codegym/blog/controller/PostController.java
+++ src/main/java/com/codegym/blog/controller/PostController.java
@@ -5,8 +5,14 @@ import com.codegym.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @Controller
 public class PostController {
@@ -27,5 +33,45 @@ public class PostController {
         return modelAndview;
     }
 
+    @GetMapping("/create-post")
+    public ModelAndView showFormCreatePost() {
+        ModelAndView modelAndView = new ModelAndView("/post/create");
+        modelAndView.addObject("post", new Post());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-post")
+    public ModelAndView createPost(@ModelAttribute("post") Post post) {
+        Date now = new Date();
+        post.setCreatedDate(now);
+        postService.save(post);
+        ModelAndView modelAndView = new ModelAndView("/post/create");
+        modelAndView.addObject("message", "Create post successful");
+        return modelAndView;
+    }
+
+    @GetMapping("/update-post/{id}")
+    public ModelAndView showFormUpdatePost(@PathVariable("id") Long id) {
+        Post post = postService.findById(id);
+        ModelAndView modelAndView;
+        if(post != null) {
+            modelAndView = new ModelAndView("/post/update");
+            modelAndView.addObject("post", post);
+        } else {
+            modelAndView = new ModelAndView("/404");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/update-post")
+    public ModelAndView updatePost(@ModelAttribute("post") Post post) {
+        Date now = new Date();
+        post.setCreatedDate(now);
+
+        postService.save(post);
+        ModelAndView modelAndView = new ModelAndView("/post/update");
+        modelAndView.addObject("message", "Update post successful");
+        return modelAndView;
+    }
 
 }
