e6b0203569d2291cd66ee242f569ba62ab167529
nguyenxuanhoa94 <nguyenxuanhoa.fullstack@gmail.com>
2018-06-18 10:55:44 +0700

    Add API find post by id

--- src/main/java/com/codegym/blog/controller/api/PostAPIController.java
+++ src/main/java/com/codegym/blog/controller/api/PostAPIController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(value = "*")
@@ -25,6 +22,16 @@ public class PostAPIController {
         Iterable<Post> posts = postService.findAll(pageable);
         if(posts != null) {
             return new ResponseEntity<>(posts, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable("id") Long id) {
+        Post post = postService.findById(id);
+        if(post != null) {
+            return new ResponseEntity<>(post, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
