dca34b69821da317734d44e1cbd0439af8757953
nguyenxuanhoa94 <nguyenxuanhoa.fullstack@gmail.com>
2018-06-18 09:30:26 +0700

    Add API find posts in category

--- src/main/java/com/codegym/blog/controller/api/CategoryAPIController.java
+++ src/main/java/com/codegym/blog/controller/api/CategoryAPIController.java
@@ -1,8 +1,11 @@
 package com.codegym.blog.controller.api;
 
 import com.codegym.blog.model.Category;
+import com.codegym.blog.model.Post;
 import com.codegym.blog.service.CategoryService;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,10 +29,17 @@ public class CategoryAPIController {
     public ResponseEntity<Category> findById(@PathVariable("id") Long id) {
         Category category = categoryService.findById(id);
         if(category != null) {
-            return new ResponseEntity<Category>(category, HttpStatus.OK);
+            return new ResponseEntity<>(category, HttpStatus.OK);
         } else {
-            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/{id}/posts")
+    public ResponseEntity<Iterable<Post>> findAllPost(@PathVariable("id") Long id, Pageable pageable) {
+        Category category = categoryService.findById(id);
+        Iterable<Post> posts = categoryService.findPosts(category, pageable);
+        return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
 }
