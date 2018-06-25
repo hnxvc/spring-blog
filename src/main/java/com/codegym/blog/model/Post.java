37a083552e02bc337a9d9ef375f358f266d41ddd
nguyenxuanhoa94 <nguyenxuanhoa.fullstack@gmail.com>
2018-06-25 10:40:58 +0700

    Add screenshot

--- src/main/java/com/codegym/blog/model/Post.java
+++ src/main/java/com/codegym/blog/model/Post.java
@@ -13,6 +13,7 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
+    @Lob
     private String description;
     @Lob
     private String content;
